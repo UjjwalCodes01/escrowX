6d1718ebbd04b40012f71dab153c5afe5b19767e
Ujjwal Tyagi <ujjwaltyagi9605@gmail.com>
2025-12-30 21:57:10 +0530

    chanhes

--- contract/src/escrow.sol
+++ contract/src/escrow.sol
@@ -8,18 +8,28 @@ contract escrow {
     constructor(){
         sender = msg.sender;
     }
-
+    event amountDeposit(address to , uint256 amount);
+    event amountWithdrawal(address from , uint256 amount);
     modifier onlySender(){
         require(msg.sender == sender , "you are not the sender");
         _;
     }
 
     function deposit(address reciever) public payable onlySender {
-        payer[reciever] = payer[reciever] + msg.value;
+        uint256 value = msg.value;
+        payer[reciever] = payer[reciever] + value;
+        emit amountDeposit(reciever, value);
     }
 
     function withdrawal(address payable reciever) public onlySender {
-        
+        uint amount = payer[reciever];
+        payer[reciever] =0;
+       (bool success ,)  = reciever.call{
+            value : amount
+        }("");
+        require(success , "transfer failed");
+        emit amountWithdrawal(reciever, amount);
     }
+
 
 }
